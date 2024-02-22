5d59714689ca8473120666fddd4ca1834dd96490
Sri Varsha Naharajan <snaharaj@iu.edu>
2024-02-22 16:53:32 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import Features from './components/Features.jsx';
+import Home from './pages/Home.jsx';
 
-import Header from './components/Header.jsx'; // Assuming you have a Header component
+//import Header from './components/Header.jsx'; // Assuming you have a Header component
+import Hero  from './components/Hero.jsx';
+import Navigation from './components/Navigation.jsx';
 import LoginSignup from './components/LoginSignup.jsx';
 import Welcome from './pages/Welcome.jsx';
 
 const App = () => {
   return (
+  
     <BrowserRouter>
-      <Header />
+      {/* <div className="bg-black"><Navigation /></div> */}
       <GoogleOAuthProvider clientId="263174241117-n1j7q8eqn323sr1s0p3i6t637589c8um.apps.googleusercontent.com">
         <Routes>
-          <Route path="/" element={<LoginSignup />} />
-          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/" element={< Home />} />
+          <Route path="/SignIn" element={<LoginSignup />} />
+          {/* <Route path="/welcome" element={<Welcome />} /> */}
+          
+          
+          
           {/* Other routes */}
         </Routes>
       </GoogleOAuthProvider>
---
+++ src/components/Navigationbar.jsx
@@ -0,0 +1,77 @@
+
+import React, { useState } from 'react';
+import { Link } from "react-scroll";
+import { FaTimes } from "react-icons/fa";
+import { Link as RouterLink } from 'react-router-dom';
+
+import { CgMenu } from 'react-icons/cg';
+
+const Navigationbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    const content = (
+        <div className='lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition'>
+            <ul className='text-center text-xl p-20'>
+                <Link spy={true} smooth={true} duration={500} to="Home" offset={-70}>
+                    <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Home</li>
+                </Link>
+                <Link spy={true} smooth={true} duration={500} to="About" offset={-70}>
+                    <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>About</li>
+                </Link>
+                <Link spy={true} smooth={true} duration={500} to="Contact" offset={-70}>
+                    <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Contact</li>
+                </Link>
+                {/* <Link spy={true} smooth={true} duration={500} to="SignIn" offset={-70}>
+                    <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Sign In</li>
+                </Link> */}
+                <RouterLink to="/signin">
+                    <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>
+                        Sign In
+                    </li>
+                </RouterLink>
+
+            </ul>
+        </div>
+    );
+
+    return (
+        <nav style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
+            <div className='h-10vh flex justify-between z-50 text-white bg-black lh:py-5 px-20 py-4'>
+                <div className='flex items-center flex-1'>
+                    <span> <h1 className='w-full text-3xl font-bold text-[#00df9a]'>DeliverEase</h1></span>
+                </div>
+                <div className='lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden'>
+                    <ul className='flex gap-8 mr-16 text-[18px]'>
+                        <Link spy={true} smooth={true} duration={500} to="Home" offset={-70}>
+                            <li className='hover:text-[#00df9a] transition border-b-2 border-slate-900 p-4 hover:border-[#00df9a] cursor-pointer'>Home</li>
+                        </Link>
+                        <Link spy={true} smooth={true} duration={500} to="About" offset={-70}>
+                            <li className='hover:text-[#00df9a] transition border-b-2 border-slate-900 p-4 hover:border-[#00df9a] cursor-pointer'>About</li>
+                        </Link>
+                        <Link spy={true} smooth={true} duration={500} to="Contact" offset={-70}>
+                            <li className='hover:text-[#00df9a] transition border-b-2 border-slate-900 p-4 hover:border-[#00df9a] cursor-pointer'>Contact</li>
+                        </Link>
+                        {/* <Link spy={true} smooth={true} duration={500} to="LoginSignup" offset={-70}>
+                            <li className='hover:text-[#00df9a] transition border-b-2 border-slate-900 p-4 hover:border-[#00df9a] cursor-pointer'>Sign In</li>
+                        </Link>  */}
+                        <RouterLink to="/signin">
+                        <li className='hover:text-[#00df9a] transition border-b-2 border-slate-900 p-4 hover:border-[#00df9a] cursor-pointer'>
+                             Sign In
+                        </li>
+                        </RouterLink>
+
+                    </ul>
+                </div>
+                <div>
+                    {click && content}
+                </div>
+                <button className='block sm:hidden translation' onClick={handleClick}>
+                    {click ? <FaTimes /> : <CgMenu />}
+                </button>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigationbar;
