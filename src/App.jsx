8e3669825d7da501dbbfafa61e43fe7c852fddbd
Sri Varsha Naharajan <snaharaj@iu.edu>
2024-03-03 22:29:18 -0500

    sprint 2 changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Home from './pages/Home.jsx';
 import Service from './pages/Service.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 import OrderForm from './components/OrderForm.jsx';
 import LoginSignup from './components/LoginSignup.jsx';
-
+import Orders from './pages/Orders.jsx'
 import Chatbot from './components/ChatBot.jsx';
+import Drivers from './pages/Drivers.jsx'
+import NewEmp from './pages/NewEmp.jsx';
+import MapView from './pages/MapView.jsx';
 
 const App = () => {
   return (
@@ -17,6 +21,12 @@ const App = () => {
           <Route path="/SignIn" element={<LoginSignup />} />
           <Route path="/service" element={<Service />} />
           <Route path="/order" element={<OrderForm />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Orders" element={<Orders />} />
+          <Route path="/Employees" element={<Drivers />} />
+          <Route path="/new" element={<NewEmp />} />
+          <Route path="/map" element={<MapView />} />
+
         </Routes>
       </GoogleOAuthProvider>
       <Chatbot /> {/* Add the chatbot to your app */}
