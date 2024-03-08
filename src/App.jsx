cf863bc62d4c50818cce7bd8041ee9aa7ac91e4d
Ismail Ben seddik <ismail@Ismails-MacBook-Pro.local>
2024-03-08 18:34:57 -0500

    varsha - admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Home from './pages/Home.jsx';
 import Service from './pages/Service.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 import OrderForm from './components/OrderForm.jsx';
 import LoginSignup from './components/LoginSignup.jsx';
-
+import Orders from './pages/Orders.jsx'
 import Chatbot from './components/ChatBot.jsx';
+import Drivers from './pages/Drivers.jsx'
+import NewEmp from './pages/NewEmp.jsx';
+import MapView from './pages/MapView.jsx';
 
 const App = () => {
   return (
@@ -14,9 +18,15 @@ const App = () => {
       <GoogleOAuthProvider clientId="263174241117-n1j7q8eqn323sr1s0p3i6t637589c8um.apps.googleusercontent.com">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/SignIn" element={<LoginSignup />} />
+          <Route path="/signin" element={<LoginSignup />} />
           <Route path="/service" element={<Service />} />
           <Route path="/order" element={<OrderForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/employees" element={<Drivers />} />
+          <Route path="/new" element={<NewEmp />} />
+          <Route path="/map" element={<MapView />} />
+
         </Routes>
       </GoogleOAuthProvider>
       <Chatbot /> {/* Add the chatbot to your app */}
--- src/components/LoginSignup.jsx
+++ src/components/LoginSignup.jsx
@@ -64,9 +64,10 @@ import { GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate for redirection
 import Dashboard from '../pages/Dashboard'
 
-import user_icon from '../Assets/person.png';
-import email_icon from '../Assets/email.png';
-import password_icon from '../Assets/password.png';
+
+import user_icon from '../assets/person.png';
+import email_icon from '../assets/email.png';
+import password_icon from '../assets/password.png';
 
 const LoginSignup = () => {
   const [action, setAction] = useState('Login');
@@ -75,6 +76,14 @@ const LoginSignup = () => {
   const [password, setPassword] = useState('');
   const navigate = useNavigate(); // Create a navigate function
   const [signedUpUsername, setSignedUpUsername] = useState('');
+  const [userType, setUserType] = useState('user'); // Default to 'user'
+  const [securityQuestion, setSecurityQuestion] = useState('');
+  const [securityAnswer, setSecurityAnswer] = useState('');
+  const securityQuestions = [
+    "What was your first pet's name?",
+    "What was the model of your first car?",
+    "In what city were you born?",
+  ];
 
   const handleSignUp = async () => {
     const response = await fetch('http://localhost:3000/api/auth/signup', {
@@ -85,9 +94,25 @@ const LoginSignup = () => {
       body: JSON.stringify({
         username,
         email,
-        password
+        password,
+        securityQuestion,
+        securityAnswer,        
       })
-    });
+    }); 
+    {/*try {
+      // Your async operation
+      const response = await fetch('http://localhost:3000/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email,
+          password
+          // userType is determined on the server, hence not included in the request
+        })
+      }); */}
+
   
     if (response.ok) {
       setSignedUpUsername(username); // Set the signed-up username
@@ -95,7 +120,22 @@ const LoginSignup = () => {
     } else {
       // Handle errors
       console.error('Signup failed');
+    } 
+    {/*if (response.ok) {
+      const data = await response.json();
+      // Check the userType and navigate accordingly
+      if (data.userType === 'admin') {
+        navigate('/admin-dashboard'); // Navigate to the admin dashboard
+      } else {
+        // For regular users, redirect to a welcome page or similar
+        navigate('/welcome', { state: { username: data.username } });
+      }
+    } else {
+      console.error('Login failed');
     }
+  } catch (error) {
+    console.error('There was a problem with the login request:', error);
+  } */}
   };
 
   return (
@@ -106,12 +146,75 @@ const LoginSignup = () => {
           <div className="underline h-1 bg-purple-700 mt-2"></div>
         </div>
         <div className="inputs space-y-4">
+
+        {/*{action === "Sign Up" && (
+            <>
+              <div className="input flex items-center space-x-2">
+                <img src={user_icon} alt="" />
+                <input type="text" placeholder="Name" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500" value={username} onChange={(e) => setUsername(e.target.value)} />
+              </div>
+              <div className="input flex items-center space-x-2">
+                <img src={email_icon} alt="" />
+                <input type="email" placeholder="Email Id" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500" value={email} onChange={(e) => setEmail(e.target.value)} />
+              </div>
+              <div className="input flex items-center space-x-2">
+                <img src={password_icon} alt="" />
+                <input type="password" placeholder="Password" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <div className="input flex items-center space-x-2">
+                <select
+                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  value={securityQuestion}
+                  onChange={(e) => setSecurityQuestion(e.target.value)}
+                >
+                  <option value="">Select a security question</option>
+                  {securityQuestions.map((question, index) => (
+                    <option key={index} value={question}>{question}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="input flex items-center space-x-2">
+                <input
+                  type="text"
+                  placeholder="Security Question Answer"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  value={securityAnswer}
+                  onChange={(e) => setSecurityAnswer(e.target.value)}
+                />
+              </div>
+            </>
+                  )}*/}
           {action === "Login" ? null : (
+            <>
             <div className="input flex items-center space-x-2">
               <img src={user_icon} alt="" />
               <input type="text" placeholder="Name" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500" value={username} onChange={(e) => setUsername(e.target.value)} />
             </div>
+            
+            <div className="input flex items-center space-x-2">
+            <select
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+              value={securityQuestion}
+              onChange={(e) => setSecurityQuestion(e.target.value)}
+            >
+              <option value="">Select a security question</option>
+              {securityQuestions.map((question, index) => (
+                <option key={index} value={question}>{question}</option>
+              ))}
+            </select>
+          </div>
+          <div className="input flex items-center space-x-2">
+            <input
+              type="text"
+              placeholder="Security Question Answer"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+              value={securityAnswer}
+              onChange={(e) => setSecurityAnswer(e.target.value)}
+            />
+          </div>
+          </>
           )}
+
 
           <div className="input flex items-center space-x-2">
             <img src={email_icon} alt="" />
@@ -134,6 +237,15 @@ const LoginSignup = () => {
           )}
 
           {action === "Login" && (
+            <>
+            <div className="user-type flex items-center space-x-2">
+
+          <select className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+  value={userType} onChange={(e) => setUserType(e.target.value)} >
+             <option value="user">User</option>
+             <option value="admin">Admin</option>
+           </select>
+           </div>
             <div className="google-login mt-4">
               <GoogleLogin
                 onSuccess={credentialResponse => {
@@ -144,6 +256,7 @@ const LoginSignup = () => {
                 }}
               />
             </div>
+            </>
           )}
 
           <div className="submit-container mt-4">
@@ -165,4 +278,4 @@ const LoginSignup = () => {
   );
 }
 
-export default LoginSignup;
+export default LoginSignup; 
