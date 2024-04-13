44c034fb48ed44dc1db3107987fe93827d529b2a
radhikaganesh29 <radhikaganesh29@gmail.com>
2024-04-13 19:35:14 -0400

    formatted service page

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -107,116 +107,114 @@ const Service = () => {
     <div>
       <LoginNavBar /> {/* Include the navigation bar component */}
       <div className="mt-24">
-
-      <div className="p-4">
-        <div className="flex items-center mb-4">
-          <input
-            type="text"
-            placeholder="Search services..."
-            className="border border-gray-300 rounded p-2 mr-2 flex-grow"
-            value={searchTerm}
-            onChange={handleInputChange}
-          />
-          <button
-            className="bg-blue-500 text-white rounded p-2"
-            onClick={handleSearch}
-          >
-            Search
-          </button>
-        </div>
-        <div className="bg-black p-4 rounded mb-4">
-          <div className="flex items-center mb-2">
+        <div className="p-4">
+          <div className="flex items-baseline mb-4">
             <input
               type="text"
-              placeholder="Title"
-              className="border border-gray-300 rounded p-2 mr-2"
-              value={filter.title}
-              onChange={(e) => updateFilter('title', e.target.value)}
+              placeholder="Search services..."
+              className="border border-gray-300 rounded p-2 mr-2 flex-grow"
+              value={searchTerm}
+              onChange={handleInputChange}
             />
-            <div className="relative">
+            <button
+              className="bg-black hover:bg-gray-400 text-[#00df9a] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-2"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
+          </div>
+          <div className="bg-black p-4 rounded mb-4">
+            <div className="flex items-baseline mb-2">
               <input
                 type="text"
-                placeholder="Company"
+                placeholder="Title"
                 className="border border-gray-300 rounded p-2 mr-2"
-                value={filter.company}
-                onChange={(e) => updateFilter('company', e.target.value)}
-                onFocus={() => setShowCompanyDropdown(true)}
+                value={filter.title}
+                onChange={(e) => updateFilter('title', e.target.value)}
               />
-              {showCompanyDropdown && (
-                <ul ref={companyDropdownRef} className="absolute bg-white border border-gray-300 rounded w-full mt-1">
-                  {availableServices
-                    .filter((service) => service.toLowerCase().startsWith(filter.company.toLowerCase()))
-                    .map((service, index) => (
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Company"
+                  className="border border-gray-300 rounded p-2 mr-2"
+                  value={filter.company}
+                  onChange={(e) => updateFilter('company', e.target.value)}
+                  onFocus={() => setShowCompanyDropdown(true)}
+                />
+                {showCompanyDropdown && (
+                  <ul ref={companyDropdownRef} className="absolute bg-white border border-gray-300 rounded w-full mt-1">
+                    {availableServices
+                      .filter((service) => service.toLowerCase().startsWith(filter.company.toLowerCase()))
+                      .map((service, index) => (
+                        <li
+                          key={index}
+                          className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                          onClick={() => handleSelectCompany(service)}
+                        >
+                          {service}
+                        </li>
+                      ))}
+                  </ul>
+                )}
+              </div>
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Type"
+                  className="border border-gray-300 rounded p-2 mr-2"
+                  value={filter.type}
+                  onChange={(e) => updateFilter('type', e.target.value)}
+                  onFocus={() => setShowTypeDropdown(true)}
+                />
+                {showTypeDropdown && (
+                  <ul ref={typeDropdownRef} className="absolute bg-white border border-gray-300 rounded w-full mt-1">
+                    {availableTypes.map((type, index) => (
                       <li
                         key={index}
                         className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-                        onClick={() => handleSelectCompany(service)}
+                        onClick={() => handleSelectType(type)}
                       >
-                        {service}
+                        {type}
                       </li>
                     ))}
-                </ul>
-              )}
-            </div>
-            <div className="relative">
+                  </ul>
+                )}
+              </div>
               <input
-                type="text"
-                placeholder="Type"
+                type="number"
+                placeholder="Min Price"
                 className="border border-gray-300 rounded p-2 mr-2"
-                value={filter.type}
-                onChange={(e) => updateFilter('type', e.target.value)}
-                onFocus={() => setShowTypeDropdown(true)}
+                value={filter.minPrice}
+                onChange={(e) => updateFilter('minPrice', e.target.value)}
               />
-              {showTypeDropdown && (
-                <ul ref={typeDropdownRef} className="absolute bg-white border border-gray-300 rounded w-full mt-1">
-                  {availableTypes.map((type, index) => (
-                    <li
-                      key={index}
-                      className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-                      onClick={() => handleSelectType(type)}
-                    >
-                      {type}
-                    </li>
-                  ))}
-                </ul>
-              )}
+              <input
+                type="number"
+                placeholder="Max Price"
+                className="border border-gray-300 rounded p-2 mr-2"
+                value={filter.maxPrice}
+                onChange={(e) => updateFilter('maxPrice', e.target.value)}
+              />
+              <button
+                className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded border border-black focus:outline-none focus:shadow-outline mt-2"
+                onClick={handleFilter}
+              >
+                Filter
+              </button>
             </div>
-            <input
-              type="number"
-              placeholder="Min Price"
-              className="border border-gray-300 rounded p-2 mr-2"
-              value={filter.minPrice}
-              onChange={(e) => updateFilter('minPrice', e.target.value)}
-            />
-            <input
-              type="number"
-              placeholder="Max Price"
-              className="border border-gray-300 rounded p-2 mr-2"
-              value={filter.maxPrice}
-              onChange={(e) => updateFilter('maxPrice', e.target.value)}
-            />
-            <button
-              className="bg-green-500 text-white rounded p-2"
-              onClick={handleFilter}
-            >
-              Filter
-            </button>
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            {services.map((service) => (
+              <div key={service._id} className="border border-gray-300 rounded p-4">
+                <h3 className="font-bold">{service.deliveryServiceTitle}</h3>
+                <p>Type: {service.deliverServiceType}</p>
+                <p>Description: {service.deliveryServiceDescription}</p>
+                <p>Company: {service.deliverServiceCompany}</p>
+                <p>Price: ${service.deliverServicePrice}</p>
+              </div>
+            ))}
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-4">
-          {services.map((service) => (
-            <div key={service._id} className="border border-gray-300 rounded p-4">
-              <h3 className="font-bold">{service.deliveryServiceTitle}</h3>
-              <p>Type: {service.deliverServiceType}</p>
-              <p>Description: {service.deliveryServiceDescription}</p>
-              <p>Company: {service.deliverServiceCompany}</p>
-              <p>Price: ${service.deliverServicePrice}</p>
-
-            </div>
-          ))}
-        </div>
       </div>
-    </div>
     </div>
   );
 };
