73222bec3b268bd1441c729f0b9131bfda427f17
radhikaganesh29 <radhikaganesh29@gmail.com>
2024-04-13 18:30:39 -0400

    formatting user profile

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -6,8 +6,8 @@ const HelpButton = () => {
   return (
     <Link
       to="/faq"
-      className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-4" // Adjusted margin to position beside Edit button
-    >
+      className="bg-black hover:bg-gray-400 text-[#00df9a] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-2">
+    
       Help
     </Link>
   );
@@ -66,26 +66,18 @@ const UserProfile = () => {
       <LoginNavBar />
       <div className="mt-4 sm:mt-24">
         <div className="container mx-auto p-4">
-          <div className="bg-green-100 rounded-lg shadow-md p-4">
-            <h2 className="text-2xl font-bold mb-4">User Profile</h2> 
-            <div className="flex justify-between items-center mb-4"> {/* Adjusted container with flex */}
-              <div> {/* Grouping the Edit and Save buttons */}
-                <button onClick={toggleEditing} className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                  {editing ? 'Cancel' : 'Edit'}
-                </button>
-                {editing && (
-                  <button onClick={saveChanges} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-4">
-                    Save
-                  </button>
-                )}
-              </div>
-              
-              <HelpButton /> {/* Display the Help button separately */}
+          {/* User Profile */}
+          <div className="bg-gray-100 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold mb-4 text-[#00df9a] bg-black p-2 rounded-lg">User Profile</h2> 
+            {/* Editing Controls */}
+            <div className="mb-4">
+              <HelpButton />
             </div>
+            {/* User Details */}
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              {/* First Name */}
               <div>
-                <p className="text-gray-700">First Name:</p>
-                {editing ? (
+                <p className="text-black"><span className="font-bold">First Name:</span> {editing ? (
                   <input
                     type="text"
                     name="firstName"
@@ -94,12 +86,12 @@ const UserProfile = () => {
                     className="form-input mt-1 w-full"
                   />
                 ) : (
-                  <p className="font-semibold">{editedUser.firstName}</p>
-                )}
+                  <span>{editedUser.firstName}</span>
+                )}</p>
               </div>
+              {/* Last Name */}
               <div>
-                <p className="text-gray-700">Last Name:</p>
-                {editing ? (
+                <p className="text-black"><span className="font-bold">Last Name:</span> {editing ? (
                   <input
                     type="text"
                     name="lastName"
@@ -108,12 +100,12 @@ const UserProfile = () => {
                     className="form-input mt-1 w-full"
                   />
                 ) : (
-                  <p className="font-semibold">{editedUser.lastName}</p>
-                )}
+                  <span>{editedUser.lastName}</span>
+                )}</p>
               </div>
-              <div className="col-span-2">
-                <p className="text-gray-700">Street:</p>
-                {editing ? (
+              {/* Street */}
+              <div>
+                <p className="text-black"><span className="font-bold">Street:</span> {editing ? (
                   <input
                     type="text"
                     name="street"
@@ -122,12 +114,12 @@ const UserProfile = () => {
                     className="form-input mt-1 w-full"
                   />
                 ) : (
-                  <p className="font-semibold">{editedUser.street}</p>
-                )}
+                  <span>{editedUser.street}</span>
+                )}</p>
               </div>
+              {/* Apt */}
               <div>
-                <p className="text-gray-700">Apt:</p>
-                {editing ? (
+                <p className="text-black"><span className="font-bold">Apt:</span> {editing ? (
                   <input
                     type="text"
                     name="apt"
@@ -136,12 +128,12 @@ const UserProfile = () => {
                     className="form-input mt-1 w-full"
                   />
                 ) : (
-                  <p className="font-semibold">{editedUser.apt}</p>
-                )}
+                  <span>{editedUser.apt}</span>
+                )}</p>
               </div>
+              {/* Zipcode */}
               <div>
-                <p className="text-gray-700">Zipcode:</p>
-                {editing ? (
+                <p className="text-black"><span className="font-bold">Zipcode:</span> {editing ? (
                   <input
                     type="text"
                     name="zipcode"
@@ -150,38 +142,47 @@ const UserProfile = () => {
                     className="form-input mt-1 w-full"
                   />
                 ) : (
-                  <p className="font-semibold">{editedUser.zipcode}</p>
+                  <span>{editedUser.zipcode}</span>
+                )}</p>
+              </div>
+              {/* Email */}
+              <div>
+                <p className="text-black"><span className="font-bold">Email:</span> {editedUser.email}</p>
+              </div>
+              {/* Mobile Number */}
+              <div>
+                <p className="text-black"><span className="font-bold">Mobile Number:</span> {editedUser.mobile}</p>
+                {/* Edit Button */}
+                <button onClick={toggleEditing} className="bg-black hover:bg-gray-400 text-[#00df9a] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-2">
+                  {editing ? 'Cancel' : 'Edit'}
+                </button>
+                {editing && (
+                  <button onClick={saveChanges} className="bg-black hover:bg-gray-400 text-[#00df9a] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-2">
+                    Save
+                  </button>
                 )}
-              </div>
-              <div className="col-span-2">
-                <p className="text-gray-700">Email:</p>
-                <p className="font-semibold">{editedUser.email}</p>
-              </div>
-              <div className="col-span-2">
-                <p className="text-gray-700">Mobile Number:</p>
-                <p className="font-semibold">{editedUser.mobile}</p>
               </div>
             </div>
           </div>
 
           {/* Spending Statistics */}
-          <div className="mt-8 bg-white rounded-lg shadow-md p-4">
-            <h2 className="text-2xl font-bold mb-4">Spending Statistics</h2>
+          <div className="mt-8 bg-gray-100 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold mb-4 text-[#00df9a] bg-black p-2 rounded-lg">Spending Statistics</h2>
             <div className="p-2">
-              <p className="text-gray-700"><span className="font-semibold">Total Spent:</span> ${totalSpent.toFixed(2)}</p>
-              <p className="text-gray-700"><span className="font-semibold">Average Order Value:</span> ${averageSpent.toFixed(2)}</p>
+              <p className="text-black"><span className="font-semibold">Total Spent:</span> ${totalSpent.toFixed(2)}</p>
+              <p className="text-black"><span className="font-semibold">Average Order Value:</span> ${averageSpent.toFixed(2)}</p>
             </div>
           </div>
             
           {/* Recent Orders */}
-          <div className="mt-8 bg-white rounded-lg shadow-md p-4">
-            <h2 className="text-2xl font-bold mb-4">Recent Orders</h2>
+          <div className="mt-8 bg-gray-100 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold mb-4 text-[#00df9a] bg-black p-2 rounded-lg">Recent Orders</h2>
             {recentOrders.map(order => (
               <div key={order.id} className="mb-4 border-b">
-                <p className="text-gray-700"><span className="font-semibold">Order ID:</span> {order.id}</p>
-                <p className="text-gray-700"><span className="font-semibold">Date:</span> {order.date}</p>
-                <p className="text-gray-700"><span className="font-semibold">Status:</span> {order.status}</p>
-                <p className="text-gray-700"><span className="font-semibold">Items:</span> {order.items.join(", ")}</p>
+                <p className="text-black"><span className="font-semibold">Order ID:</span> {order.id}</p>
+                <p className="text-black"><span className="font-semibold">Date:</span> {order.date}</p>
+                <p className="text-black"><span className="font-semibold">Status:</span> {order.status}</p>
+                <p className="text-black"><span className="font-semibold">Items:</span> {order.items.join(", ")}</p>
               </div>
             ))}
           </div>
